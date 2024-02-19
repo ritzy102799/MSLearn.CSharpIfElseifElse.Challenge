55a9cefc3a0c86fecfcbe1366ae03854b2bc4aea
ROVIC ROSALES <ritzy102799@gmail.com>
2024-02-19 17:33:10 +0100

    done project perfectly

--- MSLearn.CSharpIfElseifElse.Challenge/Program.cs
+++ MSLearn.CSharpIfElseifElse.Challenge/Program.cs
@@ -1,4 +1,6 @@
-﻿Random random = new Random();
+﻿using System.ComponentModel.Design;
+
+Random random = new Random();
 int daysUntilExpiration = random.Next(12);
 int discountPercentage = 0;
 
@@ -21,3 +23,15 @@ else if (daysUntilExpiration <= 10)
 {
     Console.WriteLine("Your subscription will expire soon. Renew now!");
 }
+
+
+
+if (daysUntilExpiration == 1)
+{    discountPercentage = 20;
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");  
+}
+else if ((daysUntilExpiration <= 5) && (daysUntilExpiration > 0))
+{
+    discountPercentage = 10;
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+ }
